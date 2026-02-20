42c341803720aa5eaeba2b0ff6bfa858f8820e7c
Samiul2024 <mdsamiullahossen@gmail.com>
2026-02-20 23:47:46 +0600

    practicing on careerCodeProject...

---
+++ src/api/applicstionsApi.js
@@ -0,0 +1,8 @@
+export const myApplicationsPromise = (email, accessToken) => {
+    return fetch(`http://localhost:3000/applications?email=${email}`, {
+        headers: {
+            authorization: `Bearer ${accessToken}`
+        }
+    })
+        .then(res => res.json())
+}
